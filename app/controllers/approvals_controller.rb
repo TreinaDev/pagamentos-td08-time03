1d612ab146a6aa21fd20342f6855f4ba3cbd5286
murichristopher <murichristopher@outlook.com>
2022-06-14 12:31:57 -0300

    feat: lógica de filtro de admins pendentes criada
    Co-authored-by: Sergio <sergiogurgell@gmail.com>

--- app/controllers/approvals_controller.rb
+++ app/controllers/approvals_controller.rb
@@ -2,14 +2,27 @@ class ApprovalsController < ApplicationController
   before_action :authenticate_admin!
 
   def index
-    @pending_admins = Admin.where("activation != ?", 10)
+    #unir as duas
+
+    pending_admins = Admin.where("activation != ?", 10)
+    #mostra todos os admins recém criados que não estejam aprovados
+
+    requested_admins = pending_admins.joins(:approvals).where.not(approvals: {super_admin_email: current_admin.email})
+    #consulta todos os admins que possuam relação com o model APPROVAL e que ainda não estejam aprovados pelo current_admin
+
+    #### criar consulta com os admins que ainda nao possuem um model APPROVAL
+    admins_without_approval = pending_admins.where(activation: "no_approval")
+
+    @arrayfinal = requested_admins + admins_without_approval
+    #Mostra na view o arrayfinal
   end
 
   def new
-    puts params
     admin = Admin.find(params[:id])
 
+
     a = Approval.create!(admin: admin, super_admin_email: current_admin.email)
+
     if admin.half_approval?
       admin.approved!
     else
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,16 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
 # Examples:
-#
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-# first_admin = Admin.create!(full_name: 'Maria', cpf: '12344444901', email: 'maria@userubis.com.br', password: '123456')
-# second_admin = Admin.create!(full_name: 'Gabriel', cpf: '10123478901', email: 'gabriel@userubis.com.br', password: '123456')
-#  Admin.create!(full_name: 'Mateus Cézar', cpf: '99023478901', email: 'matheus@userubis.com.br', password: '123456')
-#  Admin.create!(full_name: 'Fábio Júnior', cpf: '91211278901', email: 'fabiojr@userubis.com.br', password: '123456')
+first_admin = Admin.create!(full_name: 'Maria', cpf: '12344444901', email: 'maria@userubis.com.br', password: '123456')
+second_admin = Admin.create!(full_name: 'Gabriel', cpf: '10123478901', email: 'gabriel@userubis.com.br', password: '123456')
+ Admin.create!(full_name: 'Mateus Cézar', cpf: '99023478901', email: 'matheus@userubis.com.br', password: '123456')
+ Admin.create!(full_name: 'Fábio Júnior', cpf: '91211278901', email: 'fabiojr@userubis.com.br', password: '123456')
 
-# first_admin.approved!
-# second_admin.approved!
+first_admin.approved!
+second_admin.approved!
 
 
 Admin.create!(full_name: 'JOnata', cpf: '99122228921', email: 'sajdasd@userubis.com.br', password: '123456')
