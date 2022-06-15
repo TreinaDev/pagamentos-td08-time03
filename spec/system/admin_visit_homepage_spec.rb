ab0ff5dd8d7bedb0868a0b6511a092aa55032979
murichristopher <murichristopher@outlook.com>
2022-06-15 17:45:02 -0300

    refactor: adiciona homepage para Admins não aprovados
    Co-authored-by: Sergio Gurgel <sergiogurg@users.noreply.github.com>

---
+++ spec/system/admin_visit_homepage_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+describe 'Admin visita a página inicial' do
+  it 'está pendente de aprovação, e é solicitado login' do
+    admin = create(:admin)
+    admin.not_approved!
+    login_as(admin)
+
+    visit root_path
+
+    expect(page).to have_content("Sua conta ainda falta ser ativada.")
+  end
+
+  it 'possui uma aprovação, e é solicitado login' do
+    admin = create(:admin)
+    admin.half_approved!
+    login_as(admin)
+
+    visit root_path
+
+    expect(page).to have_content("Sua conta ainda falta ser ativada.")
+  end
+
+  it 'é totalmente aprovado e consegue acessar a aplicação' do
+    admin = create(:admin)
+    admin.approved!
+    login_as(admin)
+
+    visit root_path
+
+    expect(page).to have_content("Pagamentos")
+    within('nav') do
+      expect(page).to have_link("Início")
+      expect(page).to have_link("Aprovações Pendentes")
+      expect(page).to have_content("Olá, joao@userubis.com.br")
+      expect(page).to have_button("Sair")
+      expect(page).to have_link("Cotação de Rubis")
+    end
+  end
+end
