27910602f6f1cb69b9a13e04e8053ddf02ed54fb
Mat <mateuscolvr@gmail.com>
2022-06-15 17:59:19 -0300

    feat: Adiciona visualização da taxa de câmbio atual
    Co-authored-by: Fábio <fabiojrfla@gmail.com>
    Co-authored-by: Mateus <mateuscolvr@gmail.com>

--- app/models/exchange_rate.rb
+++ app/models/exchange_rate.rb
@@ -1,4 +1,9 @@
 class ExchangeRate < ApplicationRecord
   validates :real, presence: true
   validates :real, numericality: true
+
+  def self.fluctuation
+    former, latter = ExchangeRate.last(2)
+    "#{(((latter.real / former.real) - 1) * 100).round(2)}%"
+  end
 end
