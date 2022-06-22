e923ea3b0c9f1d264c516273b47d3f36d626f13e
murichristopher <murichristopher@outlook.com>
2022-06-22 18:11:32 -0300

    feat: adiciona testes unitários

--- spec/models/client_category_spec.rb
+++ spec/models/client_category_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe ClientCategory, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+ describe 'validations' do
+    context 'presence' do
+      it { should validate_presence_of(:name) }
+      it { should validate_presence_of(:discount) }
+    end
+    context "numericality" do
+      it { should validate_numericality_of(:discount).is_greater_than(0) }
+    end
+  end
 end
