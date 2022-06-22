67bc292e57ea8186e27dcab3953e0c108e5c3816
murichristopher <murichristopher@outlook.com>
2022-06-22 10:50:35 -0300

    refactor: corrige teste de visualização

--- spec/system/exchange_rates/admin_views_to_approval_exchange_rate_spec.rb
+++ spec/system/exchange_rates/admin_views_to_approval_exchange_rate_spec.rb
@@ -7,13 +7,23 @@ describe 'Admin visualiza taxas pendentes de aprovação%' do
     second_admin = create(:admin, :approved)
     create(:exchange_rate, admin: first_admin)
     create(:exchange_rate, admin: first_admin, real: 11)
+    create(:exchange_rate, admin: first_admin, real: 18)
+    create(:exchange_rate, admin: second_admin, real: 200)
 
     login_as(second_admin)
     visit exchange_rates_path
     click_on 'Taxas pendentes'
 
-    expect(page).to have_content("fernando@userubis.com.br")
-    expect(page).to have_content("R$ 10,00")
-
+    within(".pending_exchange_rate-0") do
+      expect(page).to have_content("fernando@userubis.com.br")
+      expect(page).to have_content("R$ 18,00")
+      expect(page).to have_button("Aprovar")
+    end
+    within(".pending_exchange_rate-1") do
+      expect(page).to have_content("fernando@userubis.com.br")
+      expect(page).to have_content("R$ 11,00")
+      expect(page).to have_button("Aprovar")
+    end
+    expect(page).not_to have_content("R$ 200,00")
   end
 end
