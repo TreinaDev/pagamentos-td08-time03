0ad4a18e6d736d9653421904d2645e41be892df0
Sergio Gurgel <sergiogurgell@gmail.com>
2022-06-27 12:36:40 -0300

    feat: Admin consulta saldo por um CPF/CNPJ sem pontos/traços/barras

--- app/controllers/client_balances_controller.rb
+++ app/controllers/client_balances_controller.rb
@@ -1,5 +1,8 @@
 class ClientBalancesController < ApplicationController
   def index
-    @client = Client.find_by(registration_number: params[:registration_number])
+    if params[:registration_number].present?
+      reg_numb = params[:registration_number]
+      @client = NoSymbolsRegistrationNumberSearcher.new(reg_numb).search
+    end
   end
 end
--- spec/system/client_balance/admin_consults_client_balance_spec.rb
+++ spec/system/client_balance/admin_consults_client_balance_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe 'Administrador consulta o saldo de um cliente' do
   it 'a partir da tela inicial' do
     admin = create(:admin, :approved)
-    
+
     login_as(admin)
     visit root_path
     click_on('Saldo de Clientes')
@@ -25,8 +25,7 @@ describe 'Administrador consulta o saldo de um cliente' do
     create(:credit, real_amount: 650, exchange_rate: er, client: client, company: company)
 
     login_as(admin)
-    visit root_path
-    click_on('Saldo de Clientes')
+    visit client_balances_path
     fill_in('CPF/CNPJ', with: '123.456.789-00')
     click_on('Consultar')
 
@@ -34,4 +33,28 @@ describe 'Administrador consulta o saldo de um cliente' do
     expect(page).to have_content(client.name)
     expect(page).to have_content('Saldo: RB 115.0 (R$ 1.150,00)')
   end
+
+  it 'e aceita o CPF/CNPJ sem os pontos, traços e barras' do
+    admin = create(:admin, :approved)
+    er = create(:exchange_rate, :approved, admin: admin)
+    company = create(:company)
+    client = create(:client)
+    create(:credit, real_amount: 500, exchange_rate: er, client: client, company: company)
+    create(:credit, real_amount: 650, exchange_rate: er, client: client, company: company)
+
+    login_as(admin)
+    visit client_balances_path
+    fill_in('CPF/CNPJ', with: '12345678900')
+    click_on('Consultar')
+
+    expect(page).to have_content('123.456.789-00')
+  end
+
+  # it 'mas não aceita formatos inválidos de CPF/CNPJ' do
+
+  # end
+
+  # it 'mas não encontra nenhum cliente correspondente àquele CPF/CNPJ' do
+
+  # end
 end
