6fe330435e085d7f5a55d1113b6ee9d0e2d1beca
Mat <mateuscolvr@gmail.com>
2022-06-29 12:08:43 -0300

    test(api): testes unitários para o model Order

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    context 'presence' do
+      it { should validate_presence_of(:order_code) }
+      it { should validate_presence_of(:transaction_total_value) }
+    end
+  end
+
+  describe 'associations' do
+    context 'belongs_to' do
+      it { should belong_to(:client) }
+      it { should belong_to(:exchange_rate) }
+    end
+  end
+
+  describe 'enum' do
+    it { should define_enum_for(:status).with_values({ pending: 0, approved: 5, rejected: 10 }) }
+  end
 end
