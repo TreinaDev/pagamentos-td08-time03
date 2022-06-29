a5334235b908c9de94170deff4d50c7c6aa8d54c
Sergio Gurgel <sergiogurgell@gmail.com>
2022-06-29 11:27:57 -0300

    feat: Cadastro de conversão bônus com categoria e período já contemplados e refatorações
    Co-authored-by: Fabio Santos Jr. <fabiojrfla@gmail.com>

--- spec/system/bonus_conversion/admin_registers_bonus_conversion_spec.rb
+++ spec/system/bonus_conversion/admin_registers_bonus_conversion_spec.rb
@@ -50,9 +50,27 @@ describe 'Admin registra uma conversão bônus' do
     fill_in 'Prazo de uso',	with: '5'
     click_on 'Criar'
 
-    expect(page).to have_content('Erro ao criar categoria de clientes')
+    expect(page).to have_content('Erro ao criar conversão bônus')
     expect(page).to have_content('Categoria de cliente é obrigatório(a)')
     expect(page).to have_content('Bônus não pode ficar em branco')
     expect(page).to have_content('Data inicial deve ser menor que a data final')
+  end
+
+  it 'com categoria e período já contemplados' do
+    admin = create(:admin, :approved)
+    client_categ = create(:client_category, name: 'PREMIUM')
+    create(:bonus_conversion, start_date: '2022-06-23', end_date: '2022-10-11', bonus_percentage: '12.5', deadline: 4, client_category: client_categ)
+
+    login_as(admin)
+    visit new_bonus_conversion_path
+    fill_in 'Data inicial', with: '01/07/2022'
+    fill_in 'Data final', with: '01/10/2022'
+    fill_in 'Bônus',	with: '30'
+    fill_in 'Prazo de uso',	with: '7'
+    select 'PREMIUM', from: 'bonus_conversion_client_category_id'
+    click_on 'Criar'
+
+    expect(page).to have_content('Erro ao criar conversão bônus')
+    expect(page).to have_content('Período já contemplado para essa categoria')
   end
 end
