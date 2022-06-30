5c10d5e5a75c71f09dbe5837cea1508d5953245d
murichristopher <murichristopher@outlook.com>
2022-06-30 12:53:09 -0300

    chore: adiciona orders controller

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,25 @@
+class OrdersController < ApplicationController
+  before_action :set_order, only: %i[approve reject]
+  def index
+    @orders = Order.where(status: 0).order(created_at: :desc)
+  end
+
+  def approve
+    debit = Debit.new(real_amount: @order.transaction_total_value, client: @order.client,
+                      exchange_rate: ExchangeRate.current, order: @order)
+    debugger
+    redirect_to orders_path, notice: 'Pedido aprovado com sucesso!' if debit.save
+  end
+
+  def reject
+    return unless @order.rejected!
+
+    redirect_to orders_path, alert: 'Pedido reprovado com sucesso!'
+  end
+
+  private
+
+  def set_order
+    @order = Order.find(params[:order_id])
+  end
+end
